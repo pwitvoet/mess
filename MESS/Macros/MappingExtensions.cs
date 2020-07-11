ad1d7cdb19ec5db8baa4fb75db09dbe78035c1e0
Pieter Witvoet <p.witvoet@gmail.com>
2020-07-11 21:37:19 +0200

    Renamed MapExtensions to MappingExtensions.

--- MESS/Macros/MappingExtensions.cs
+++ MESS/Macros/MappingExtensions.cs
@@ -1,11 +1,14 @@
 ﻿using MESS.Mapping;
+using MESS.Spatial;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace MESS.Macros
 {
-    static class MapExtensions
+    static class MappingExtensions
     {
+        /// <summary>
+        /// Selects all entities with the specified class name.
+        /// </summary>
         public static IEnumerable<Entity> GetEntitiesWithClassName(this Map map, string className) => map.Entities.Where(entity => entity.ClassName == className);
-    }
 }
