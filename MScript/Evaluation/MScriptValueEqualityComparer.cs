93fef06d659a2a3fc00a0f593df80d0b14a6f95c
Pieter Witvoet <p.witvoet@gmail.com>
2023-06-16 23:06:38 +0200

    Forgot to add MScriptValueEqualityComparer.cs.

---
+++ MScript/Evaluation/MScriptValueEqualityComparer.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace MScript.Evaluation
+{
+    internal class MScriptValueEqualityComparer : IEqualityComparer<object?>
+    {
+        public static MScriptValueEqualityComparer Instance { get; } = new MScriptValueEqualityComparer();
+
+
+        public new bool Equals(object? left, object? right) => Operations.IsTrue(Operations.Equals(left, right));
+
+        public int GetHashCode([DisallowNull] object? obj) => obj?.GetHashCode() ?? 0;
+    }
+}
