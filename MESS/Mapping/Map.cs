0a914b40cb0ec2adb41705cfae446e1a6c935627
Pieter Witvoet <p.witvoet@gmail.com>
2024-02-02 00:30:15 +0100

    Added a few comments to Map properties.

--- MESS/Mapping/Map.cs
+++ MESS/Mapping/Map.cs
@@ -33,15 +33,31 @@ namespace MESS.Mapping
 
 
         // RMF/JMF formats only:
+        /// <summary>
+        /// A list of all entity paths. These can be expanded into sets of connected path entities.
+        /// </summary>
         public List<EntityPath> EntityPaths { get; } = new();
 
 
         // Common editor state:
+        /// <summary>
+        /// A cordon area can be used to export and compile a specific part of a map.
+        /// </summary>
         public BoundingBox? CordonArea { get; set; }
 
+        /// <summary>
+        /// A list of all groups in this map. This includes groups that are part of other groups.
+        /// </summary>
         public List<Group> Groups { get; } = new();
+
+        /// <summary>
+        /// A list of all visibility groups in this map. Visibility groups are used in level editors to quickly hide or show related objects.
+        /// </summary>
         public List<VisGroup> VisGroups { get; } = new();
 
+        /// <summary>
+        /// A list of cameras. Cameras are only used in level editors.
+        /// </summary>
         public List<Camera> Cameras { get; } = new();
         public int? ActiveCameraIndex { get; set; }
 
