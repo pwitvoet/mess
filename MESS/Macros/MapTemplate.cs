pwitvoet/mess

commit 7bea0a432bc74b5968d8cf448feeee6c166f8828

Added a warning log message if a macro_template or macro_brush entity does not have an origin brush, while its anchor point is set to origin brush.

MESS/Macros/MapTemplate.cs
@@ -98,7 +98,14 @@ namespace MESS.Macros
                 var templateArea = templateEntity.BoundingBox.ExpandBy(0.5f);
                 var templateName = templateEntity.Properties.EvaluateToString(Attributes.Targetname, mapContext);
                 var selectionWeight = templateEntity.Properties.EvaluateToString(Attributes.SelectionWeight, mapContext);
-                var offset = GetTemplateEntityOrigin(templateEntity, mapContext) * -1;
+
+                if (!Enum.TryParse<TemplateAreaAnchor>(templateEntity.Properties.EvaluateToString(Attributes.Anchor, context), out var anchor))
+                    anchor = TemplateAreaAnchor.Bottom;
+
+                if (anchor == TemplateAreaAnchor.OriginBrush && templateEntity.GetOrigin() == null)
+                    logger.Warning($"Template '{templateName}' in map '{path}' has no origin! Add an origin brush, or use a different anchor point.");
+
+                var offset = templateEntity.GetAnchorPoint(anchor);
                 var templateMap = new Map();
 
                 // Copy custom properties into the template map properties - these will serve as local variables that will be evaluated whenever the template is instantiated:
@@ -147,14 +154,6 @@ namespace MESS.Macros
             }
 
             return subTemplates;
-        }
-
-        private static Vector3D GetTemplateEntityOrigin(Entity templateEntity, EvaluationContext context)
-        {
-            if (!Enum.TryParse<TemplateAreaAnchor>(templateEntity.Properties.EvaluateToString(Attributes.Anchor, context), out var anchor))
-                anchor = TemplateAreaAnchor.OriginBrush;
-
-            return templateEntity.GetAnchorPoint(anchor);
         }
 
         /// <summary>

MESS/Macros/MacroExpander.cs
@@ -983,7 +983,11 @@ namespace MESS.Macros
             // TODO: Transform! A macro_brush can be part of a normal template, and so it should propagate transform to its entities!
             //       Verify that this works correctly now!!!
             var parentID = context.GetNextParentID();
-            var anchorPoint = brushEntity.GetAnchorPoint(brushEntity.Properties.GetEnum<TemplateAreaAnchor>(Attributes.Anchor) ?? TemplateAreaAnchor.Bottom);
+            var anchor = brushEntity.Properties.GetEnum<TemplateAreaAnchor>(Attributes.Anchor) ?? TemplateAreaAnchor.Bottom;
+            if (anchor == TemplateAreaAnchor.OriginBrush && brushEntity.GetOrigin() == null)
+                Logger.Warning($"{brushEntity.ClassName} '{brushEntity.Properties.GetString(Attributes.Targetname)}' in instance #{context.ID} has no origin brush! Add an origin brush, or use a different anchor point.");
+
+            var anchorPoint = brushEntity.GetAnchorPoint(anchor);
             var anchorOffset = evaluatedProperties.GetVector3D(Attributes.InstanceOffset) ?? new Vector3D();
 
             var transform = new Transform(context.Transform.Scale, context.Transform.GeometryScale, context.Transform.Rotation, context.Transform.Apply(anchorPoint + anchorOffset));

commit ee1c4c6c84510d1e70d8f5aec43b4d350e41e0dd

Fixed a template offset bug that was introduced in 7bea0a432bc74b5968d8cf448feeee6c166f8828.

MESS/Macros/MapTemplate.cs
@@ -105,7 +105,7 @@ namespace MESS.Macros
                 if (anchor == TemplateAreaAnchor.OriginBrush && templateEntity.GetOrigin() == null)
                     logger.Warning($"Template '{templateName}' in map '{path}' has no origin! Add an origin brush, or use a different anchor point.");
 
-                var offset = templateEntity.GetAnchorPoint(anchor);
+                var offset = -templateEntity.GetAnchorPoint(anchor);
                 var templateMap = new Map();
 
                 // Copy custom properties into the template map properties - these will serve as local variables that will be evaluated whenever the template is instantiated:
@@ -150,7 +150,6 @@ namespace MESS.Macros
                     case Entity entity: map.RemoveEntity(entity); break;
                     case Brush brush: map.RemoveBrush(brush); break;
                 }
-                // TODO: They're also still part of groups, vis-groups, etc!
             }
 
             return subTemplates;