pwitvoet/mess

commit a6e2239e69e98376e31ea9d008f2013ca60aabd4

Strings in entity definitions in .ted files can now contain MScript expressions. Standard functions and rewrite rule functions are available.

MESS/EntityRewriting/RewriteDirectiveParser.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.RegularExpressions;
+﻿using MESS.Macros;
+using MScript;
+using System.Text.RegularExpressions;
 
 namespace MESS.EntityRewriting
 {
@@ -16,14 +18,19 @@ namespace MESS.EntityRewriting
     /// </summary>
     public static class RewriteDirectiveParser
     {
-        public static IEnumerable<RewriteDirective> ParseRewriteDirectives(Stream stream)
+        public static IEnumerable<RewriteDirective> ParseRewriteDirectives(Stream tedStream, TextWriter fgdOutput, EvaluationContext evaluationContext)
         {
             var input = "";
-            using (var reader = new StreamReader(stream, leaveOpen: true))
+            using (var reader = new StreamReader(tedStream, leaveOpen: true))
                 input = reader.ReadToEnd();
 
-            var tokens = FgdTokenizer.Tokenize(input);
-            using (var context = new Context(tokens.Where(token => token.Type != FgdTokenizer.TokenType.Comment)))
+            // First copy the .ted file contents to the .fgd output:
+            var tokens = FgdTokenizer.Tokenize(input).ToArray();
+            foreach (var token in tokens)
+                WriteToken(token, fgdOutput, evaluationContext);
+
+            // Then parse the .ted file to extract rewrite directives:
+            using (var context = new Context(tokens.Where(token => token.Type != FgdTokenizer.TokenType.Comment && token.Type != FgdTokenizer.TokenType.Whitespace)))
             {
                 RewriteDirective? unassociatedRewriteDirective = null;
                 while (!context.IsExhausted)
@@ -59,6 +66,28 @@ namespace MESS.EntityRewriting
             }
         }
 
+
+        private static void WriteToken(FgdTokenizer.Token token, TextWriter fgdOutput, EvaluationContext evaluationContext)
+        {
+            switch (token.Type)
+            {
+                case FgdTokenizer.TokenType.String:
+                    fgdOutput.Write('"');
+                    fgdOutput.Write(Evaluation.EvaluateInterpolatedString(token.Value, evaluationContext));
+                    fgdOutput.Write('"');
+                    break;
+
+                case FgdTokenizer.TokenType.Comment:
+                case FgdTokenizer.TokenType.MessDirective:
+                    fgdOutput.Write("//");
+                    fgdOutput.Write(token.Value);
+                    break;
+
+                default:
+                    fgdOutput.Write(token.Value);
+                    break;
+            }
+        }
 
         /// <summary>
         /// Parses the next entity definition and returns its name.

MESS/EntityRewriting/FgdTokenizer.cs
@@ -10,7 +10,10 @@ namespace MESS.EntityRewriting
             var isMessDirectiveEnabled = false;
             while (true)
             {
-                SkipWhitespace(context);
+                var whitespaceToken = ReadWhitespace(context);
+                if (whitespaceToken != null)
+                    yield return whitespaceToken.Value;
+
                 if (context.IsExhausted)
                     yield break;
 
@@ -42,10 +45,22 @@ namespace MESS.EntityRewriting
         }
 
 
-        private static void SkipWhitespace(Context context)
+        private static Token? ReadWhitespace(Context context)
         {
+            var line = context.Line;
+            var offset = context.Offset;
+
+            var buffer = new StringBuilder();
             while (!context.IsExhausted && char.IsWhiteSpace(context.Current))
+            {
+                buffer.Append(context.Current);
                 context.MoveNext();
+            }
+
+            if (buffer.Length > 0)
+                return new Token(line, offset, TokenType.Whitespace, buffer.ToString());
+            else
+                return null;
         }
 
         private static Token ReadToken(Context context)
@@ -53,18 +68,19 @@ namespace MESS.EntityRewriting
             var line = context.Line;
             var offset = context.Offset;
 
+            var currentChar = context.Current;
             switch (context.Current)
             {
-                case ',': context.MoveNext(); return new Token(line, offset, TokenType.Comma, context.Current.ToString());
-                case '=': context.MoveNext(); return new Token(line, offset, TokenType.Assignment, context.Current.ToString());
-                case ':': context.MoveNext(); return new Token(line, offset, TokenType.Colon, context.Current.ToString());
-                case '-': context.MoveNext(); return new Token(line, offset, TokenType.Minus, context.Current.ToString());
-                case '(': context.MoveNext(); return new Token(line, offset, TokenType.ParensOpen, context.Current.ToString());
-                case ')': context.MoveNext(); return new Token(line, offset, TokenType.ParensClose, context.Current.ToString());
-                case '[': context.MoveNext(); return new Token(line, offset, TokenType.BracketOpen, context.Current.ToString());
-                case ']': context.MoveNext(); return new Token(line, offset, TokenType.BracketClose, context.Current.ToString());
-                case '{': context.MoveNext(); return new Token(line, offset, TokenType.BraceOpen, context.Current.ToString());
-                case '}': context.MoveNext(); return new Token(line, offset, TokenType.BraceClose, context.Current.ToString());
+                case ',': context.MoveNext(); return new Token(line, offset, TokenType.Comma, currentChar.ToString());
+                case '=': context.MoveNext(); return new Token(line, offset, TokenType.Assignment, currentChar.ToString());
+                case ':': context.MoveNext(); return new Token(line, offset, TokenType.Colon, currentChar.ToString());
+                case '-': context.MoveNext(); return new Token(line, offset, TokenType.Minus, currentChar.ToString());
+                case '(': context.MoveNext(); return new Token(line, offset, TokenType.ParensOpen, currentChar.ToString());
+                case ')': context.MoveNext(); return new Token(line, offset, TokenType.ParensClose, currentChar.ToString());
+                case '[': context.MoveNext(); return new Token(line, offset, TokenType.BracketOpen, currentChar.ToString());
+                case ']': context.MoveNext(); return new Token(line, offset, TokenType.BracketClose, currentChar.ToString());
+                case '{': context.MoveNext(); return new Token(line, offset, TokenType.BraceOpen, currentChar.ToString());
+                case '}': context.MoveNext(); return new Token(line, offset, TokenType.BraceClose, currentChar.ToString());
 
                 case '"':
                     return ReadString(context);
@@ -196,6 +212,8 @@ namespace MESS.EntityRewriting
             BracketClose,   // ]
             BraceOpen,      // {
             BraceClose,     // }
+
+            Whitespace,     // \s+
         }
 
 

MESS/EntityRewriting/RewriteDirective.cs
@@ -8,9 +8,10 @@
 
 
     /// <summary>
-    /// Entity rewrite rules are used to modify matching entities immediately after a map file is loaded.
+    /// Entity rewrite rules are used to modify matching entities immediately after a map file is loaded, or after the main map has been processed.
     /// The intended use-case is to turn custom entities into macro entities that reference a specific template map.
     /// However, rewrite rules can be used to overwrite or delete any attribute, not just the classname.
+    /// Rewrite rules operate on unevaluated keys and values.
     /// </summary>
     public class RewriteDirective
     {

MESS/Macros/MacroExpander.cs
@@ -1237,7 +1237,7 @@ namespace MESS.Macros
         }
 
 
-        class MacroExpanderFunctions
+        public class MacroExpanderFunctions
         {
             private string _templatesDirectory;
             private string _messDirectory;
@@ -1283,7 +1283,7 @@ namespace MESS.Macros
         }
 
 
-        class RewriteDirectiveFunctions
+        public class RewriteDirectiveFunctions
         {
             private string _sourceFilePath;
 

MESS/Program.cs
@@ -84,7 +84,9 @@ namespace MESS
                     logger.Important($"Arguments: {string.Join(" ", Environment.GetCommandLineArgs())}");
                     logger.Important("");
 
-                    var rewriteDirectives = LoadTedRewriteDirectives(settings.TemplateEntityDirectories, settings.MessFgdFilePath, logger);
+                    var rewriteDirectivesEvaluationContext = Evaluation.DefaultContext();
+                    NativeUtils.RegisterInstanceMethods(rewriteDirectivesEvaluationContext, new MacroExpander.MacroExpanderFunctions(settings.TemplateMapsDirectory, AppContext.BaseDirectory, settings.Globals));
+                    var rewriteDirectives = LoadTedRewriteDirectives(settings.TemplateEntityDirectories, settings.MessFgdFilePath, rewriteDirectivesEvaluationContext, logger);
 
                     try
                     {
@@ -248,9 +250,9 @@ namespace MESS
         /// This reads rewrite directives from all .ted files (which are basically small .fgd files) in the template directories (including .ted files inside .zip files).
         /// It will also update mess.fgd, if it's outdated.
         /// </summary>
-        private static RewriteDirective[] LoadTedRewriteDirectives(IEnumerable<string> templateDirectories, string messFgdFilePath, ILogger logger)
+        private static RewriteDirective[] LoadTedRewriteDirectives(IEnumerable<string> templateDirectories, string messFgdFilePath, EvaluationContext baseEvaluationContext, ILogger logger)
         {
-            var messFgdBuffer = new StringBuilder();
+            var fgdOutput = new StringWriter();
             var rewriteDirectives = new List<RewriteDirective>();
 
             foreach (var templatesDirectory in templateDirectories)
@@ -265,45 +267,35 @@ namespace MESS
                 {
                     var directives = ZipFileSystem.ReadFiles(templatesDirectory, ".ted", (file, path) =>
                         {
-                            using (var memoryStream = new MemoryStream())
+                            try
                             {
-                                file.CopyTo(memoryStream);
+                                // Write .ted file information to mess.fgd:
+                                fgdOutput.Write("// ");
+                                fgdOutput.WriteLine(new string('=', path.Length));
+                                fgdOutput.WriteLine($"// {path}");
+                                fgdOutput.Write("// ");
+                                fgdOutput.WriteLine(new string('=', path.Length));
+                                fgdOutput.WriteLine();
 
-                                try
-                                {
-                                    // First read the rewrite rules:
-                                    memoryStream.Position = 0;
-                                    var rewriteDirectives = RewriteDirectiveParser.ParseRewriteDirectives(memoryStream).ToArray();
-                                    foreach (var rewriteDirective in rewriteDirectives)
-                                        rewriteDirective.SourceFilePath = path;
+                                var tedEvaluationContext = new EvaluationContext(parentContext: baseEvaluationContext);
+                                NativeUtils.RegisterInstanceMethods(tedEvaluationContext, new MacroExpander.RewriteDirectiveFunctions(path));
 
-                                    logger.Info($"{rewriteDirectives.Length} rewrite directives read from '{path}'.");
+                                // Then read the rewrite rules, and copy the .ted file contents into mess.fgd:
+                                var rewriteDirectives = RewriteDirectiveParser.ParseRewriteDirectives(file, fgdOutput, tedEvaluationContext).ToArray();
+                                foreach (var rewriteDirective in rewriteDirectives)
+                                    rewriteDirective.SourceFilePath = path;
 
-                                    // Then, if the rewrite rules were read successfully, copy the file content into the mess.fgd content buffer:
-                                    memoryStream.Position = 0;
-                                    using (var streamReader = new StreamReader(memoryStream, leaveOpen: true))
-                                    {
-                                        if (messFgdBuffer.Length > 0)
-                                            messFgdBuffer.AppendLine();
+                                fgdOutput.WriteLine();
+                                fgdOutput.WriteLine();
 
-                                        messFgdBuffer.Append("// ");
-                                        messFgdBuffer.AppendLine(new string('=', path.Length));
-                                        messFgdBuffer.AppendLine($"// {path}");
-                                        messFgdBuffer.Append("// ");
-                                        messFgdBuffer.AppendLine(new string('=', path.Length));
-                                        messFgdBuffer.AppendLine();
+                                logger.Info($"{rewriteDirectives.Length} rewrite directives read from '{path}'.");
 
-                                        messFgdBuffer.Append(streamReader.ReadToEnd());
-                                        messFgdBuffer.AppendLine();
-                                    }
-
-                                    return rewriteDirectives;
-                                }
-                                catch (Exception ex)
-                                {
-                                    logger.Warning($"Failed to read rewrite directives from '{path}':", ex);
-                                    return Array.Empty<RewriteDirective>();
-                                }
+                                return rewriteDirectives;
+                            }
+                            catch (Exception ex)
+                            {
+                                logger.Warning($"Failed to read rewrite directives from '{path}':", ex);
+                                return Array.Empty<RewriteDirective>();
                             }
                         })
                         .SelectMany(rewriteRules => rewriteRules)
@@ -315,7 +307,7 @@ namespace MESS
             }
 
 
-            UpdateMessFgd(messFgdFilePath, messFgdBuffer.ToString(), logger);
+            UpdateMessFgd(messFgdFilePath, fgdOutput.ToString(), logger);
 
             return rewriteDirectives.ToArray();
         }

commit 2cf4bb94c15c90cd5fe138be5cf3fb77c930f694

Fixed that MESS directives and comments were missing newlines in mess.fgd, which could result in a broken .fgd file (bug was introduced in a6e2239e69e98376e31ea9d008f2013ca60aabd4).

MESS/EntityRewriting/RewriteDirectiveParser.cs
@@ -81,7 +81,7 @@ namespace MESS.EntityRewriting
                 case FgdTokenizer.TokenType.Comment:
                 case FgdTokenizer.TokenType.MessDirective:
                     fgdOutput.Write("//");
-                    fgdOutput.Write(token.Value);
+                    fgdOutput.WriteLine(token.Value);
                     break;
 
                 default: