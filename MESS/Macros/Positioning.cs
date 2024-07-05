8926fd227503e2544bb77e3ca6a3ff9bb1654be6
Pieter Witvoet <p.witvoet@gmail.com>
2024-07-05 16:51:29 +0200

    Two more forgotten files...

---
+++ MESS/Macros/Positioning.cs
@@ -0,0 +1,15 @@
+﻿namespace MESS.Macros
+{
+    enum Positioning
+    {
+        /// <summary>
+        /// Instance offsets are relative to the position of the parent macro entity.
+        /// </summary>
+        Relative,
+
+        /// <summary>
+        /// Instance offsets are treated as absolute positions.
+        /// </summary>
+        Absolute,
+    }
+}
---
+++ MESS/Macros/Scaling.cs
@@ -0,0 +1,15 @@
+﻿namespace MESS.Macros
+{
+    enum Scaling
+    {
+        /// <summary>
+        /// Instance scales are relative to the scale of the parent macro entity.
+        /// </summary>
+        Relative,
+
+        /// <summary>
+        /// Instance scales are absolute.
+        /// </summary>
+        Absolute,
+    }
+}
